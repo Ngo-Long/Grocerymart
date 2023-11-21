28bfb5b099a5af4374ff468b64f3803b7166e9b4
Ngo-Long <ngolong.hh@gmail.com>
2023-11-22 00:26:09 +0700

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -160,11 +160,14 @@ window.addEventListener('template-loaded', initJsToggle);
 function initJsToggle() {
   $$('.js-toggle').forEach((button) => {
     const target = button.getAttribute('toggle-target');
+
     if (!target) {
       document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
     }
 
-    button.onclick = () => {
+    button.onclick = (e) => {
+      e.preventDefault();
+
       if (!$(target)) {
         return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
       }
@@ -175,6 +178,15 @@ function initJsToggle() {
         $(target).classList.toggle('hide', !isHidden);
         $(target).classList.toggle('show', isHidden);
       });
+    };
+
+    document.onclick = function (e) {
+      if (!e.target.closest(target)) {
+        const isHidden = $(target).classList.contains('hide');
+        if (!isHidden) {
+          button.click();
+        }
+      }
     };
   });
 }
