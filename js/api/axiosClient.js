d6059656b1645413ef45e6d89ca76c62eda00edb
Ngo-Long <ngolong.hh@gmail.com>
2023-12-26 14:59:17 +0700

    Code API:
    - Fake json-server (db.json)
    - Add template HTML for a product item
    - Code file axiosClient.js
    - Code file productApi.js
    - Code fn renderProductList(data) in home.js

--- js/api/axiosClient.js
+++ js/api/axiosClient.js
@@ -7,4 +7,47 @@ const axiosClient = axios.create({
   },
 });
 
+// Add a request interceptor
+axiosClient.interceptors.request.use(
+  function (config) {
+    // attach token to request if exists
+    const accessToken = localStorage.getItem('access_token');
+    // privateRequests
+    if (accessToken) {
+      // lấy từ local storage (ko có thì chịu)
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+axiosClient.interceptors.response.use(
+  function (response) {
+    // transform data for all responses
+    return response.data;
+  },
+  function (error) {
+    console.log('axiosClient - response error', error.response);
+    if (!error.response) throw new Error('Network error. Please try again later');
+
+    // rediract to logon if not login
+    if (error.response.status === 401) {
+      // clear token, logout
+      //...
+      window.location.assign('/login.html');
+      return;
+    }
+
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
+
 export default axiosClient;
--- js/home.js
+++ js/home.js
@@ -1,11 +1,72 @@
-import axiosClient from './api/axiosClient.js';
+import productApi from './api/productApi';
 import { startCarousel } from './utils/index.js';
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+function createProductItem(dataItem) {
+  if (!dataItem) return;
+
+  // Get id product template
+  const productTemplate = $('#productTemplate');
+  if (!productTemplate) return;
+
+  // Get in content product template
+  const productItem = productTemplate.content.firstElementChild.cloneNode(true);
+  if (!productItem) return;
+
+  // Update title, brand, price, score, image, thumbList
+  const titleProduct = productItem.querySelector('[data-id="titleProduct"]');
+  if (titleProduct) titleProduct.textContent = dataItem.title;
+
+  const priceProduct = productItem.querySelector('[data-id="priceProduct"]');
+  if (priceProduct) priceProduct.textContent = dataItem.price;
+
+  const brandProduct = productItem.querySelector('[data-id="brandProduct"]');
+  if (brandProduct) brandProduct.textContent = dataItem.brand;
+
+  const scoreProduct = productItem.querySelector('[data-id="scoreProduct"]');
+  if (scoreProduct) scoreProduct.textContent = dataItem.score;
+
+  const imageUrlProduct = productItem.querySelector('[data-id="imageUrlProduct"]');
+  if (imageUrlProduct) imageUrlProduct.textContent = dataItem.imageUrl;
+
+  const isFavoriteProduct = productItem.querySelector('[data-id="isFavoriteProduct"]');
+  if (isFavoriteProduct) isFavoriteProduct.textContent = dataItem.isFavorite;
+
+  const descProduct = productItem.querySelector('[data-id="descProduct"]');
+  if (descProduct) descProduct.textContent = dataItem.description;
+
+  return productItem;
+}
+
+function renderProductList(dataList) {
+  if (!Array.isArray(dataList) || !dataList.length) return;
+
+  const productList = $('#productList');
+  if (!productList) return;
+
+  dataList.forEach((dataItem) => {
+    const productItem = createProductItem(dataItem);
+
+    productList.appendChild(productItem);
+  });
+}
+
 // Main
-(() => {
+(async () => {
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 6,
+    };
+
+    const data = await productApi.getAll(queryParams);
+    renderProductList(data);
+  } catch (error) {
+    console.log('Get all failed', error);
+  }
+
   startCarousel({
     carouselId: '#carousel-id',
     carouselItem: '.carousel__item',
@@ -13,9 +74,3 @@ const $$ = document.querySelectorAll.bind(document);
     selectorNext: 'button[data-bs-slide="next"]',
   });
 })();
-
-async function main() {
-  const response = await axiosClient.get('/products');
-  console.log(response);
-}
-main();
